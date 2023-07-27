154e42abdc422cfec5b7401bf0c08ab65bd169c0
kazmy <nehaluddin.dpe@gmail.com>
2023-07-27 13:35:30 +0530

    order Model created

--- src/model/orderModel.js
+++ src/model/orderModel.js
@@ -3,10 +3,10 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 
 const orderSchema = new mongoose.Schema({
     userId : { type: ObjectId, ref:'user', required : true },
-    items: [{
-        productName: { type: String, required : true },
-        quantity: { type: Number, required: true, min : 1 }
-    }],
+    // items: [{
+    //     productName: { type: String, required : true },
+    //     quantity: { type: Number, required: true, min : 1 }
+    // }],
     totalItem : { type : Number, required : true},
     quantity : { type : Number, required : true},
     price : { type: Number, required: true},
